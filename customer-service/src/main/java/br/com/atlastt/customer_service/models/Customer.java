2e3959529a28914bcdb6791f17915ef0cf5ba944
TonhoDevi <antoniohenriquebn@hotmail.com>
2026-08-13 21:57:54 -0300

    feat: add Customer entity

---
+++ customer-service/src/main/java/br/com/atlastt/customer_service/models/Customer.java
@@ -0,0 +1,78 @@
+package br.com.atlastt.customer_service.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 150,nullable = false)
+    private String name;
+
+    @Column(length = 150,nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 20,nullable = false, unique = true)
+    private String document;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    protected Customer() {
+    }
+
+    public Customer(long id, String name, String email, String document) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.document = document;
+    }
+
+    public Customer(String name, String email, String document) {
+        this.name = name;
+        this.email = email;
+        this.document = document;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
