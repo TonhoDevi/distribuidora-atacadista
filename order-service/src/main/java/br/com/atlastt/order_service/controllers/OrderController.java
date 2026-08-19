71a6589270065160c98267918e526a23b46e82e2
TonhoDevi <antoniohenriquebn@hotmail.com>
2026-08-19 18:18:05 -0300

    feat: add OrderController with REST endpoints

---
+++ order-service/src/main/java/br/com/atlastt/order_service/controllers/OrderController.java
@@ -0,0 +1,35 @@
+package br.com.atlastt.order_service.controllers;
+
+import br.com.atlastt.order_service.dtos.OrderRequestDto;
+import br.com.atlastt.order_service.dtos.OrderResponseDto;
+import br.com.atlastt.order_service.services.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public OrderResponseDto createOrder(@Valid @RequestBody OrderRequestDto orderRequestDto) {
+        return orderService.createOrder(orderRequestDto);
+    }
+
+    @GetMapping
+    public List<OrderResponseDto> getAllOrders() {
+        return orderService.findAllOrders();
+    }
+
+    @GetMapping("/{id}")
+    public OrderResponseDto getOrderById(@PathVariable Long id) {
+        return orderService.findOrderById(id);
+    }
+}
