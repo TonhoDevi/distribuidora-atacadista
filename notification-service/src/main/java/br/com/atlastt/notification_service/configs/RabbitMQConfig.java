2b2669546fe93b75088155b17ab7e4e190d26b13
TonhoDevi <antoniohenriquebn@hotmail.com>
2026-08-23 16:19:00 -0300

    feat(notification-service): implement order creation consumer

--- notification-service/src/main/java/br/com/atlastt/notification_service/configs/RabbitMQConfig.java
+++ notification-service/src/main/java/br/com/atlastt/notification_service/configs/RabbitMQConfig.java
@@ -1,4 +1,37 @@
 package br.com.atlastt.notification_service.configs;
 
-public class eded {
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String EXCHANGE_NAME = "pedidos.exchange";
+    public static final String QUEUE_NAME = "pedido.criado.queue";
+    public static final String ROUTING_KEY = "pedido.criado";
+
+    @Bean
+    public DirectExchange pedidosExchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue pedidoCriadoQueue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public Binding binding(Queue pedidoCriadoQueue, DirectExchange pedidosExchange) {
+        return BindingBuilder.bind(pedidoCriadoQueue)
+                .to(pedidosExchange)
+                .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
 }
